2f3cb3796de6826c92e4d10e59012484cc4f00f5
Harry Moreno <morenoh149@gmail.com>
2017-03-05 16:47:05 -0500

    stub go linalg

---
+++ linear_algebra.go
@@ -0,0 +1,118 @@
+package main
+
+func transpose(A) {
+	for i := 0; i < len(A); i++ {
+		for j := 0; j < len(A[i]); j++ {
+
+		}
+	}
+	result = A[i][j]
+	return result
+}
+
+/*
+import math
+
+
+def transpose(A):
+    return [[A[i][j] for i in range(len(A))] for j in range(len(A[0]))]
+
+
+def is_scalar(x):
+    return type(x) in [int, float]
+
+
+def is_vector(x):
+    return type(x) is list and \
+           all(is_scalar(x[i]) for i in range(len(x)))
+
+
+def is_matrix(x):
+    return type(x) is list and  \
+           all(type(x[i]) is list for i in range(len(x))) and \
+           all(len(x[i]) == len(x[0]) for i in range(len(x)))
+
+
+def add(A, B):
+    if is_matrix(A) and is_matrix(B):
+        row = len(A)
+        column = len(A[0])
+        return [[A[i][j] + B[j][i] for j in range(column)] for i in range(row)]
+    elif is_vector(A) and is_vector(B):
+        return [A[i] + B[i] for i in range(len(A))]
+
+
+def minus(A):
+    if is_matrix(A):
+        return [[-A[i][j] for j in range(len(A[0]))] for i in range(len(A))]
+    elif is_vector(A):
+        return [-A[i] for i in range(len(A))]
+
+
+def multiply(A, B):
+    if is_scalar(A) and is_vector(B):
+        return [A * B[i] for i in range(len(B))]
+    elif is_vector(A) and is_vector(B):
+        # Vector product, not element-wise product
+        return sum([A[i] * B[i] for i in range(len(A))])
+    elif is_scalar(A) and is_matrix(B):
+        I = len(B)
+        J = len(B[0])
+        return [[A*B[i][j] for j in range(J)] for i in range(I)]
+    elif is_vector(A) and is_matrix(B):
+        I = len(A)
+        J = len(B[0])
+        return [sum(A[i] * B[i][j] for i in range(I)) for j in range(J)]
+    elif is_matrix(A) and is_vector(B):
+        I = len(A)
+        J = len(A[0])
+        return [sum(A[i][j] * B[j] for j in range(J)) for i in range(I)]
+    elif is_matrix(A) and is_matrix(B):
+        I = len(A)
+        J = len(B[0])
+        K = len(A[0])
+        return [[sum(A[i][k] * B[k][j] for k in range(K)) for j in range(J)] for i in range(I)]
+    raise Exception('No match!')
+
+
+def identity(size):
+    return [[1 if i == j else 0 for j in range(size)] for i in range(size)]
+
+
+def inverse(A):
+    # 2x2 matrix only
+    determinant = A[0][0] * A[1][1] - A[0][1] * A[1][0]
+    return multiply(1./determinant, [[A[1][1], -A[0][1]], [-A[1][0], A[0][0]]])
+
+
+def norm(x):
+    return math.sqrt(sum(x[i] ** 2 for i in range(len(x))))
+
+
+def diagonal(x):
+    return [[x[i] if i == j else 0 for j in range(len(x))] for i in range(len(x))]
+
+
+def eigen_composition(V, l):
+    return multiply(multiply(V, diagonal(l)), inverse(V))
+
+
+def normalize(x):
+    return [x[i]/norm(x) for i in range(len(x))]
+
+
+def eigen_decomposition(A):
+    # 2x2 matrix only
+    l1 = (A[0][0] + A[1][1] + math.sqrt((A[0][0] - A[1][1])**2. + 4. * A[0][1] * A[1][0])) / 2.
+    l2 = (A[0][0] + A[1][1] - math.sqrt((A[0][0] - A[1][1])**2. + 4. * A[0][1] * A[1][0])) / 2.
+    v1 = [A[0][1], l1 - A[0][0]]
+    v2 = [A[0][1], l2 - A[0][0]]
+    V = transpose([normalize(v1), normalize(v2)])
+    return [[l1, l2], V]
+
+
+def principal_component_analysis(X):
+    # X: n * 2 matrix (n: number of data points)
+    return eigen_decomposition(multiply(transpose(X), X))
+
+*/
---
+++ test_linear_algebra.go
@@ -0,0 +1,101 @@
+/*
+from unittest import TestCase
+from linear_algebra import *
+
+
+class TestLinearAlgebra(TestCase):
+    def test_transpose(self):
+        A = [[1, 2], [3, 4]]
+        actual = transpose(A)
+        expected = [[1, 3], [2, 4]]
+        self.assertEqual(actual, expected)
+
+    def test_plus(self):
+        A = [[1, 2], [3, 4]]
+        B = [[1, 1], [1, 1]]
+        actual = add(A, B)
+        expected = [[2, 3], [4, 5]]
+        self.assertEqual(actual, expected)
+
+    def test_multiply(self):
+        A = [[1, 2], [3, 4]]
+        B = [[1, 1], [1, 1]]
+        actual = multiply(A, B)
+        expected = [[3, 3], [7, 7]]
+        self.assertEqual(actual, expected)
+
+        actual = multiply(2, A)
+        expected = [[2, 4], [6, 8]]
+        self.assertEqual(actual, expected)
+
+        actual = multiply([1, 2], A)
+        expected = [7, 10]
+        self.assertEqual(actual, expected)
+
+        actual = multiply(A, [1, 2])
+        expected = [5, 11]
+        self.assertEqual(actual, expected)
+
+    def test_identity(self):
+        A = [[1, 2], [3, 4]]
+        I = identity(2)
+        self.assertEqual(multiply(A, I), A)
+        self.assertEqual(multiply(I, A), A)
+
+    def test_inverse(self):
+        A = [[1, 2], [3, 4]]
+        A_inverse = inverse(A)
+        self.assertEqual(multiply(A, A_inverse), identity(2))
+        self.assertEqual(multiply(A_inverse, A), identity(2))
+
+    def test_norm(self):
+        x = [3, 4]
+        actual = norm(x)
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    def test_diagonal(self):
+        x = [1, 2]
+        actual = diagonal(x)
+        expected = [[1, 0], [0, 2]]
+        self.assertEqual(actual, expected)
+
+    def test_eigen_composition(self):
+        V = [[1, 1], [1, -1]]
+        l = [1, 2]
+        A = eigen_composition(V, l)
+        self.assertEqual(multiply(A, [1, 1]), [1, 1])
+        self.assertEqual(multiply(A, [1, -1]), multiply(2, [1, -1]))
+
+    def test_eigen_decomposition(self):
+        A = [[0., 1.], [-2., -3.]]
+        l, V = eigen_decomposition(A)
+        self.assertEqual(l, [-1., -2.])
+
+        v1, v2 = transpose(V)
+
+        for i in range(2):
+            self.assertAlmostEqual(multiply(A, v1)[i], multiply(l[0], v1)[i])
+            self.assertAlmostEqual(multiply(A, v2)[i], multiply(l[1], v2)[i])
+
+        for i in range(2):
+            for j in range(2):
+                self.assertAlmostEqual(multiply(A, V)[i][j], multiply(V, diagonal(l))[i][j])
+
+        for i in range(2):
+            for j in range(2):
+                self.assertAlmostEqual(A[i][j], eigen_composition(V, l)[i][j])
+
+    def test_principal_component_analysis(self):
+        X = [[0., 0.],
+             [1., 0.],
+             [0., 1.],
+             [1., 1.]]
+        l, D = principal_component_analysis(X)
+        C = multiply(transpose(D), transpose(X))
+        R = transpose(multiply(D, C))
+
+        for i in range(len(X)):
+            for j in range(len(X[0])):
+                self.assertAlmostEqual(X[i][j], R[i][j])
+*/
